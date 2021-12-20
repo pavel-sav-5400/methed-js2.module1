19f9d45354b4ae48d13031b3d18dc9da86e088ee
Pavel Savelev <pavel.s.20.18@mail.ru>
2021-12-20 14:35:46 +1000

    залили проект на github

---
+++ lesson_01/index.js
@@ -0,0 +1,15 @@
+'use strict'
+
+{
+   const product = 'Двигатель';
+   const quantity = 15;
+   const category = 'Автозапчасти';
+   const cost = 50000;
+
+   console.log('Наименование товара:', product);
+   console.log('Общая сумма товара:' , quantity * cost);
+}
+
+
+
+
