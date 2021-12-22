0962616f06e56cb24b36bef2742518ce7f61ae17
Pavel Savelev <pavel.s.20.18@mail.ru>
2021-12-22 19:04:32 +1000

    3я задача готова

--- lesson_04/tasks.js
+++ lesson_04/tasks.js
@@ -8,4 +8,15 @@
         if (Math.round(rain) == 0) {
             console.log('Дождя нет!');
         };
+
+
+        const rusBalls = +prompt('Введите кол-во баллов по русскому языку');
+        const matBalls = +prompt('Введите кол-во баллов по математики');
+        const infBalls = +prompt('Введите кол-во баллов по информатики');
+        
+        if (rusBalls + matBalls + infBalls > 265) {
+            console.log('Поздравляю, вы поступили на бюджет!');
+        } else {
+            console.log('К сожалению вы не поступили:(');
+        }
        
