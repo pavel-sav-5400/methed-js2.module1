f230ea5e72799f5ca05a3e2c8605448ccb3aac3e
Pavel Savelev <pavel.s.20.18@mail.ru>
2021-12-22 16:22:29 +1000

    подправил консоль

--- lesson_03/einstein.js
+++ lesson_03/einstein.js
@@ -3,4 +3,4 @@
         const myWeight = 80;
         const speedLight = 3e8;
 
-        console.log(myWeight * (speedLight ** 2));
+        console.log(`Энергия вашего тела: ${myWeight * (speedLight ** 2)}`);
