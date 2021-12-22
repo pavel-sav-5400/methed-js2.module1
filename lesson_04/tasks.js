21b3eac7dec134630d96ec2dbff2482cf6981e68
Pavel Savelev <pavel.s.20.18@mail.ru>
2021-12-22 18:51:22 +1000

    добавили файл tasks.js

---
+++ lesson_04/tasks.js
@@ -0,0 +1,11 @@
+        'use strict'
+
+        const rain = Math.random() * 1;
+        
+        if (Math.round(rain) == 1) {
+            console.log('Пошёл дождь. Возьмите зонт!');
+        } 
+        if (Math.round(rain) == 0) {
+            console.log('Дождя нет!');
+        };
+       
