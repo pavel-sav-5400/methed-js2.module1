736fc4915c6d45297ba6f3aeb4e33b7bdadff97b
Pavel Savelev <pavel.s.20.18@mail.ru>
2022-01-02 19:23:34 +1000

    task01

---
+++ lesson_06/task01.js
@@ -0,0 +1,9 @@
+        'use strict'
+
+        for (let i = 1; i < 11; i++) {
+            console.log('_____________________');
+            for (let x = 1; x < 11; x++) {
+                console.log(`${i} в степени ${x} равно ${i ** x}`);
+            }
+        }
+        
