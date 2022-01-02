d5d2e6338546daebdab374c3612b6389f0c68616
Pavel Savelev <pavel.s.20.18@mail.ru>
2022-01-02 20:14:20 +1000

    add isPrime()

---
+++ lesson_06/task02.js
@@ -0,0 +1,16 @@
+        'use strict'
+
+        const isPrime = () => {
+                for (let i = 5; i < Infinity; i++) {
+                    if (i % 2) 
+                    return true;
+               
+                    else {
+                     return false; 
+               }
+              
+            }
+            
+            
+        };
+        console.log(isPrime());
