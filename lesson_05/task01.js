0fb6095a50820f6ffa7dee75e34a2c7acb74148a
Pavel Savelev <pavel.s.20.18@mail.ru>
2022-01-05 13:58:42 +1000

    <-les04

--- lesson_05/task01.js
+++ lesson_05/task01.js
@@ -5,23 +5,8 @@
            let doll = 1.2;
            let rub = 73;
            let money = +prompt('Введите стоимость покуки в евро');
-
-                const eurToDol = () => {
-                 return money * doll
-                  
-           }; 
-           const doll1 = eurToDol();
-           //console.log(eurToDol());
-
-                const dolToRub = () => {
-                 return doll1 * rub    
-                };
-                //console.log(dolToRub());
-
-
-           return dolToRub();
-           
-        };
+                return money * doll * rub;
+               };
         
         
         
