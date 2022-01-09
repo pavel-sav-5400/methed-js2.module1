331aab6eeb29a1cfc23fe7e8c9a342476ed71fbf
Pavel Savelev <pavel.s.20.18@mail.ru>
2022-01-09 16:16:11 +1000

    правки в task02.js

--- lesson_07/task02.js
+++ lesson_07/task02.js
@@ -6,8 +6,8 @@ const allСashbox = [4500, 3210, 650, 1250, 7830, 990, 13900, 370];
 const getAverageValue = (arr) => {
     
     let n = 0;
-    for(let i = 0; i < allСashbox.length; i++)
-    n += arr[i] / allСashbox.length
+    for(let i = 0; i < arr.length; i++)
+    n += arr[i] / arr.length
     return Math.floor(n)
 };
 console.log('Средний чек за день: ', getAverageValue(allСashbox));
