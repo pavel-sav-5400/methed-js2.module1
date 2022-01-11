5bc5fbfce9afa425bd7e41cb9d4713d726eaa15f
Pavel Savelev <pavel.s.20.18@mail.ru>
2022-01-11 19:22:36 +1000

    внес правки

--- lesson_07/task01.js
+++ lesson_07/task01.js
@@ -1,59 +1,11 @@
-        'use strict'
-
-       const allStudents = ['Иванов', 'Петров', 'Сидоров', 'Кузнецов', 'Смирнов', 'Попов', 'Соколов'];    
-       const failedStudents = ['Сидоров', 'Смирнов', 'Попов'];
-      
-       const filter = (a, b) => {
-               let stud = [];
-               for(let i = 0; i < a.length; i++)
-               stud = a.filter(item => !b.includes(item))
-               return stud;
-       };
-              console.log('Студенты, сдавшие экзамен: ', filter(allStudents, failedStudents));
-
-       
-
-       
-       
-       
-       
-
-       
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-        /*const filter = (allStudents, failedStudents) => {
-
-        const student = [];
-
-       for (let i = 0; i < allStudents.length; i++) {
-         
-            
-    
-      };
-      
-      const res = filter(allStudents, failedStudents);
-      console.log('res: ', res);*/
-
-      
-      
-       
-
-        
-        
+'use strict'
+const allStudents = [
+    'Иванов', 'Петров', 'Сидоров', 'Кузнецов', 'Смирнов', 'Попов', 'Соколов'
+    ];    
+const failedStudents = ['Сидоров', 'Смирнов', 'Попов'];
+const filter = (a, b) => {
+    let stud = [];
+    stud = a.filter(item => !b.includes(item))
+    return stud;
+};
+    console.log('Студенты, сдавшие экзамен: ', filter(allStudents, failedStudents));
--- lesson_07/task03.js
+++ lesson_07/task03.js
@@ -1,14 +1,11 @@
-        'use strict'
+'use strict'
+const names= [
+    'Noah', 'Liam', 'Mason', 'Jacob', 'Robot', 'William', 'Ethan', 'Michael', 'Alexander'
+    ];
 
-        const names= ['Noah', 'Liam', 'Mason', 'Jacob', 'Robot', 'William', 'Ethan', 'Michael', 'Alexander'];
-        
-        const addPrefix = (a, b) => {
-            let x = [];
-            for(let i = 0; i < a.length; i++)
-            x = a.map(elem => b + elem);
-            return x;
-        };
-            console.log('addPrefix: ', addPrefix(names, 'Mr '));
-
-        
-
+const addPrefix = (a, b) => {
+    let x = [];
+    x = a.map(elem => b + elem);
+    return x;
+};
+    console.log('addPrefix: ', addPrefix(names, 'Mr '));
