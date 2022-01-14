be58b69d519b9746afffb4198f2f1859a25c147e
Pavel Savelev <pavel.s.20.18@mail.ru>
2022-01-14 19:15:10 +1000

    less8.2

--- lesson_08/task01.js
+++ lesson_08/task01.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use strict'
 
 const nums = [
@@ -6,26 +5,8 @@ const nums = [
 ];
 
 const numbers = nums.map(item =>  {
-    const getRundomNum = () => {
-    return Math.round(Math.random() * 100 + 1);
-    };
-    return getRundomNum();
-    })
+    let x = Math.round(Math.random() * 100 + 1);
+    return x;
+   })
  
  console.log(numbers);
-=======
-'use strict'
-
-const nums = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-];
-
-const numbers = nums.map(item =>  {
-    const getRundomNum = () => {
-    return Math.round(Math.random() * 100 + 1);
-    };
-    return getRundomNum();
-    })
- 
- console.log(numbers);
->>>>>>> 52a3d199abe34b5e35874ef373f7efb55d589cd3
--- lesson_08/task02.js
+++ lesson_08/task02.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use strict'
 
 const nums = [
@@ -6,26 +5,28 @@ const nums = [
 ];
 
 const numbers = nums.map((item, n, m) =>  {
+     let x = Math.round((Math.random() * 100 + 1));
+    return x 
+})
+console.log(numbers);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const numbers = nums.map((item, n, m,) =>  {
     const getRundomNum = (n, m) => {
     return Math.round(Math.random() * (m - n) + n);
     };
     return getRundomNum(-61, 76);
-    })
-
-console.log(numbers);
-=======
-'use strict'
-
-const nums = [
-   1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-];
-
-const numbers = nums.map((item, n, m) =>  {
-    const getRundomNum = (n, m) => {
-    return Math.round(Math.random() * (m - n) + n);
-    };
-    return getRundomNum(-61, 76);
-    })
-
-console.log(numbers);
->>>>>>> 52a3d199abe34b5e35874ef373f7efb55d589cd3
+    })*/
