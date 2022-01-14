6ade574813f70fc566880cfcef7d1691b486fab6
Pavel Savelev <pavel.s.20.18@mail.ru>
2022-01-14 15:36:52 +1000

    less8.1

--- lesson_08/task01.js
+++ lesson_08/task01.js
@@ -1,16 +1,16 @@
     'use strict'
 
  const nums = [
-    13, 7, 55, 9, 67, 89, 5, 2, 49, 68, 23, 4, 
-    98, 97, 91, 6, 1, 54, 76, 31, 8, 64, 25
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10
 ];
 
-let numbers = nums.map(item => {
-        const getRundomNum = (min, max) => {
+
+
+const numbers = nums.map(item =>  {
+    const getRundomNum = (min, max) => {
         return Math.floor(Math.random() * (max - min) + min);
     };
         return getRundomNum(1, 101);
-       
-})
+    })
  
 console.log(numbers);
---
+++ lesson_08/task02.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const nums = [
+   13, 7, 55, 9, 67, 89, 5, 2, 49, 68, 23, 4, 
+   98, 97, 91, 6, 1, 54, 76, 31, 8, 64, 25
+];
+
+let numbers = nums.map((item, n, m) => {
+       const getRundomNum = (min, max) => {
+       return Math.floor(Math.random() * (max - min) + min);
+   };
+       return getRundomNum(1, 101);
+      
+})
+
+console.log(numbers);
