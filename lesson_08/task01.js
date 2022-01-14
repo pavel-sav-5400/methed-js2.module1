aaf8aff3f514db38d7ddb90d04d26e85cb029914
Pavel Savelev <pavel.s.20.18@mail.ru>
2022-01-14 16:53:06 +1000

    less8.2

--- lesson_08/task01.js
+++ lesson_08/task01.js
@@ -4,7 +4,7 @@ const nums = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10
 ];
 
-
+console.log();
 
 const numbers = nums.map(item =>  {
     const getRundomNum = () => {
--- lesson_08/task02.js
+++ lesson_08/task02.js
@@ -15,3 +15,4 @@ const numbers = nums.map((item, n, m) =>  {
     })
 
 console.log(numbers);
+console.log();
