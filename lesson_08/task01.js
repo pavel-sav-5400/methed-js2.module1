52a3d199abe34b5e35874ef373f7efb55d589cd3
pavel-sav-5400 <88177944+pavel-sav-5400@users.noreply.github.com>
2022-01-14 17:19:00 +1000

    Add files via upload

---
+++ lesson_08/task01.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const nums = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+];
+
+const numbers = nums.map(item =>  {
+    const getRundomNum = () => {
+    return Math.round(Math.random() * 100 + 1);
+    };
+    return getRundomNum();
+    })
+ 
+ console.log(numbers);
---
+++ lesson_08/task02.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const nums = [
+   1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+];
+
+const numbers = nums.map((item, n, m) =>  {
+    const getRundomNum = (n, m) => {
+    return Math.round(Math.random() * (m - n) + n);
+    };
+    return getRundomNum(-61, 76);
+    })
+
+console.log(numbers);
