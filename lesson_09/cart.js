dd2efadaefd87cf5fb6cf9efa97ef1686d4e630f
Pavel Savelev <pavel.s.20.18@mail.ru>
2022-01-19 20:59:12 +1000

    2

--- lesson_09/cart.js
+++ lesson_09/cart.js
@@ -2,15 +2,16 @@
 
 const cart = {
     items: [
-        'Чайник', 'Фен', 'Утюг' 
+        
     ],
     totalPrice: 0,
     count: 0,
     getTotalPrice() {
         return this.totalPrice
     },
-    add() {
-        return this.items
+    add(elem, price, count) {
+        return cart.items.push(elem, price, count);
+  
     },
     increaseCount(i) {
         return this.count + i
@@ -25,10 +26,12 @@ const cart = {
 
     },
 };
-console.log(cart.getTotalPrice());
-console.log(cart.add());
-console.log(cart.increaseCount(1));
+console.log('Общая стоимость: ', cart.getTotalPrice());
+console.log('Метод add: ',  cart.add('Чайник', 4000, 3));
+console.log('Увеличение кол-ва товара: ', cart.increaseCount(1));
 console.log(cart.calculateItemPrice());
 console.log(cart.clear());
 console.log(cart.print());
+
+console.log('cart.items: ', cart.items);
 
