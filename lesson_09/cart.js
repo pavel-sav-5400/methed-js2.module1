7b0ef7032216dd1b37f558645b0da90ec4b158d1
Pavel Savelev <pavel.s.20.18@mail.ru>
2022-01-23 16:18:29 +1000

    less9

--- lesson_09/cart.js
+++ lesson_09/cart.js
@@ -1,37 +1,61 @@
 'use strict'
 
 const cart = {
-    items: [
-        
-    ],
+    items: [],
     totalPrice: 0,
     count: 0,
     getTotalPrice() {
         return this.totalPrice
     },
+
     add(elem, price, count) {
-        return cart.items.push(elem, price, count);
-  
+        this.items.push({elem, price, count});
+        this.increaseCount(1);
+        this.getTotalPrice();
     },
+
     increaseCount(i) {
-        return this.count + i
+        return this.count += i
     },
+
     calculateItemPrice() {
-
+        return this.items.map(item => item.price * item.count).reduce((a, b) => a + b)
+        
     },
+   
+
     clear() {
-
+        for (const prop of Object.keys(cart)) {
+            delete cart[prop];
+          }
     },
+
     print() {
-
+       let strItems = JSON.stringify(this.items) + 'Общая стоимость: ' + JSON.stringify(this.totalPrice)
+       
+       return strItems;
+       
     },
+    
+    
 };
+
+
+
+
+
+
+
 console.log('Общая стоимость: ', cart.getTotalPrice());
-console.log('Метод add: ',  cart.add('Чайник', 4000, 3));
-console.log('Увеличение кол-ва товара: ', cart.increaseCount(1));
+cart.add('Чайник', 3500, 3);
+cart.add('Фен', 2400, 5);
+cart.add('Микроволновка', 6300, 2);
+console.log('Увеличение кол-ва товара: ', cart.increaseCount(0));
 console.log(cart.calculateItemPrice());
-console.log(cart.clear());
+//console.log(cart.clear());
 console.log(cart.print());
+console.log('Count ', cart.count);
 
 console.log('cart.items: ', cart.items);
+
 
