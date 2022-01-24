ae728d02a75ff90e6df35223e24f85d4d18a9c06
Pavel Savelev <pavel.s.20.18@mail.ru>
2022-01-24 11:31:50 +1000

    добавил правки

--- lesson_09/cart.js
+++ lesson_09/cart.js
@@ -5,57 +5,39 @@ const cart = {
     totalPrice: 0,
     count: 0,
     getTotalPrice() {
-        return this.totalPrice
+        return this.totalPrice;
     },
-
     add(elem, price, count) {
         this.items.push({elem, price, count});
         this.increaseCount(1);
         this.getTotalPrice();
+        this.calculateItemPrice();
     },
-
     increaseCount(i) {
-        return this.count += i
+        return this.count += i;
     },
-
     calculateItemPrice() {
-        return this.items.map(item => item.price * item.count).reduce((a, b) => a + b)
-        
+        let x = 0;
+        x = this.items.map(item => item.price * item.count).reduce((a, b) => a + b);
+        return this.totalPrice = x;
     },
-   
-
     clear() {
-        for (const prop of Object.keys(cart)) {
-            delete cart[prop];
-          }
+        for (const prop in cart) {
+        if (cart.hasOwnProperty(prop))
+        cart[prop] = 0;
+        }
+       
     },
-
     print() {
-       let strItems = JSON.stringify(this.items) + 'Общая стоимость: ' + JSON.stringify(this.totalPrice)
-       
-       return strItems;
-       
+        console.log(JSON.stringify(this.items));
+        console.log('Общая стоимость: ' + JSON.stringify(this.totalPrice));
     },
-    
-    
 };
 
-
-
-
-
-
-
-console.log('Общая стоимость: ', cart.getTotalPrice());
 cart.add('Чайник', 3500, 3);
-cart.add('Фен', 2400, 5);
+cart.add('Планшет', 2400, 5);
 cart.add('Микроволновка', 6300, 2);
-console.log('Увеличение кол-ва товара: ', cart.increaseCount(0));
-console.log(cart.calculateItemPrice());
-//console.log(cart.clear());
-console.log(cart.print());
-console.log('Count ', cart.count);
-
-console.log('cart.items: ', cart.items);
-
-
+cart.add('Телефон', 2700, 4);
+cart.add('Холодильник', 15000, 1);
+//cart.clear();
+cart.print();
