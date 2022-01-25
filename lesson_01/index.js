9197154c9f6afa4b5c0c269677e4706c7e2d3bbc
Pavel Savelev <pavel.s.20.18@mail.ru>
2022-01-25 15:37:18 +1000

    исправил ошибки в cart.js

--- lesson_01/index.js
+++ lesson_01/index.js
@@ -1,15 +1,12 @@
-'use strict'
+'use strict';
 
 {
-   const product = 'Двигатель';
-   const quantity = 15;
-   const category = 'Автозапчасти';
-   const cost = 50000;
+  const product = 'Двигатель';
+  const quantity = 15;
+  // eslint-disable-next-line no-unused-vars
+  const category = 'Автозапчасти';
+  const cost = 50000;
 
-   console.log('Наименование товара:', product);
-   console.log('Общая сумма товара:' , quantity * cost);
+  console.log('Наименование товара:', product);
+  console.log('Общая сумма товара:', quantity * cost);
 }
-
-
-
-
