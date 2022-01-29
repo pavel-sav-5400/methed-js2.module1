784a2d969460f9ec3c2df55f21fe7bc5c26c8dbb
Pavel Savelev <pavel.s.20.18@mail.ru>
2022-01-29 14:06:18 +1000

    добавил setDiscont в cart.js

--- lesson_12/this/cart.js
+++ lesson_12/this/cart.js
@@ -10,8 +10,16 @@ const cart = {
   set totalPrice(value) {
     this.calculateItemPrice = value;
   },
-  setDiscount() {
-
+  get setDiscount() {
+    return this.discount;
+  },
+  set setDiscount(promo) {
+    if (promo === 'METHED') {
+      this.discount = 15;
+    }
+    if (promo === 'NEWYEAR') {
+      this.discount = 21;
+    }
   },
   add(elem, price, count) {
     this.items.push({elem, price, count});
@@ -22,7 +30,8 @@ const cart = {
   },
   calculateItemPrice() {
     return this.items.map(item =>
-      item.price * item.count).reduce((a, b) => a + b);
+    item.price * item.count).reduce((a, b) => a + b) - ((this.items.map(item =>
+    item.price * item.count).reduce((a, b) => a + b)) * this.discount / 100);
   },
   clear() {
     for (const prop in cart) {
@@ -37,7 +46,7 @@ const cart = {
     console.log('Общая стоимость: ' + JSON.stringify(this.totalPrice));
   },
 };
-
+cart.setDiscount = 'METHED';
 cart.add('Чайник', 3600, 3);
 cart.add('Планшет', 2400, 5);
 cart.add('Микроволновка', 6300, 2);
@@ -45,3 +54,4 @@ cart.add('Телефон', 2700, 4);
 cart.add('Холодильник', 15000, 1);
 // cart.clear();
 cart.print();
+
