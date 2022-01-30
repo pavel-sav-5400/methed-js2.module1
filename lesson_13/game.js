caeafc517b088f265079b8cf7068a80062670b5d
Pavel Savelev <pavel.s.20.18@mail.ru>
2022-01-30 18:36:26 +1000

    дописал условия выхода

--- lesson_13/game.js
+++ lesson_13/game.js
@@ -3,23 +3,22 @@
 (() => {
  const FIGURES_RUS = ['камень', 'ножницы', 'бумага'];
 
-
  const getRandomIntInclusive = (min, max) => {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min);
  };
  
- 
- 
- 
-        
- 
-
  const game = () => {
      const result = {
          player: 0,
          computer: 0,
+         incPlayer(i) {
+             this.player += i;
+         },
+         incComputer(i) {
+             this.computer += i;
+         },
      };
      return function start() {
          const user = prompt('Выберите: камень, ножницы или бумага?');
@@ -42,13 +41,27 @@
             alert('Ничья');
             start();
         };
-        if (user === 'камень' && comp === 'бумага' || user === 'бумага' && comp === 'ножницы' || user === 'ножницы' && comp === 'камень') {
+        if (user === 'камень' && comp === 'бумага' || user === 'бумага' 
+        && comp === 'ножницы' || user === 'ножницы' && comp === 'камень') {
             alert('Вы проиграли');
+            result.incComputer(1);
             start();
         };
-        if (user === 'камень' && comp === 'ножницы' || user === 'бумага' && comp === 'камень' || user === 'ножницы' && comp === 'бумага') {
+        if (user === 'камень' && comp === 'ножницы' || user === 'бумага' 
+        && comp === 'камень' || user === 'ножницы' && comp === 'бумага') {
             alert('Вы выиграли');
+            result.incPlayer(1);
             start();
+        }
+        if (user === null) {
+            const ques = confirm('Вы точно хотите выйти?');
+            if (ques === true) {
+                alert('Не очень то и хотелось =(' + "\n" + 'Ваши очки: ' + result.player + "\n" + 'Очки компьютера: ' + result.computer);
+            }
+            else {
+                alert('Играем дальше =)');
+                start();
+            }
         }
 
          console.log('comp: ', comp);
