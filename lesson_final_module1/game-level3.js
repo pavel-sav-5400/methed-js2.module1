0d03b88772fed7f65915a54821784a9f66252423
Pavel Savelev <pavel.s.20.18@mail.ru>
2022-02-08 10:06:36 +1000

    переписал условия для бота и жребия

--- lesson_final_module1/game-level3.js
+++ lesson_final_module1/game-level3.js
@@ -15,183 +15,138 @@
     const balls = {
       computer: 5,
       player: 5,
-      addPlayer(i) {
-        this.player += i;
+      addPlayer(usBal) {
+        this.player += usBal;
       },
-      addComputer(i) {
-        this.computer += i;
+      addComputer(compBal) {
+        this.computer += compBal;
       },
-      takePlayer(i) {
-        this.player -= i;
+      takePlayer(usBal) {
+        this.player -= usBal;
       },
-      takeComputer(i) {
-        this.computer -= i;
+      takeComputer(compBal) {
+        this.computer -= compBal;
       },
     };
 
-
     return (function start() {
-      return function firstStep() {
-        const user = prompt('Выберите: камень, ножницы или бумага?');
-        let comp = getRandomIntInclusive(0, 2);
-        const FIGURES_RUS = ['камень', 'ножницы', 'бумага'];
-        const compVal = () => {
-          if (comp === 0) {
-            comp = FIGURES_RUS[0];
-          }
-          if (comp === 1) {
-            comp = FIGURES_RUS[1];
-          }
-          if (comp === 2) {
-            comp = FIGURES_RUS[2];
-          }
-        }; compVal();
+      const user = prompt('Выберите: камень, ножницы или бумага?');
+      const comp = getRandomIntInclusive(0, 2);
 
-        if (user === comp) {
-          alert('Ничья');
-          return firstStep();
+      if (
+        (user === 'камень') && (comp === 0) ||
+      (user === 'бумага') && (comp === 2) ||
+      (user === 'ножницы') && (comp === 1)) {
+        alert('Ничья');
+        return start();
+      }
+
+      if (
+        (user === 'камень') && (comp === 2) ||
+        (user === 'бумага') && (comp === 1) ||
+        (user === 'ножницы') && (comp === 0)) {
+        return stepBot();
+      }
+
+      if (
+        (user === 'камень') && (comp === 1) ||
+        (user === 'бумага') && (comp === 0) ||
+        (user === 'ножницы') && (comp === 3)) {
+        return stepPlayer();
+      }
+      if (user === null) {
+        return alert('Пока');
+      } else {
+        return start();
+      }
+
+      function stepPlayer() {
+        if (balls.player < 1) {
+          return alert('Бот выиграл');
+        } if (balls.computer < 1) {
+          return alert('Вы выиграли');
         }
+        const usNum = prompt(`введи число от 1 до ${balls.player}`);
+        const botNum = getRandomIntInclusive(1, 2);
 
-        if (
-          (user === 'камень' || user === 'к' ||
-           user === 'кам') && comp === 'бумага' ||
-            (user === 'бумага' || user === 'бум' || user === 'б') &&
-            comp === 'ножницы' ||
-            (user === 'ножницы' || user === 'нож' || user === 'н') &&
-            comp === 'камень') {
-          return stepBot();
+        if (usNum === null) {
+          return alert('Пока');
         }
+        if (isNaN(usNum)) {
+          alert('Вы ввели не число');
+          stepPlayer();
+        }
+        if (usNum < 1) {
+          alert('Вы ввели отрицательное число');
+          stepPlayer();
+        }
+        if (usNum > balls.player) {
+          alert('Вы ввели число больше');
+          stepPlayer();
+        }
+        if ((usNum % 2 == 0) && (botNum % 2 == 0)) {
+          balls.addComputer(+usNum);
+          balls.takePlayer(+usNum);
+          alert(`Бот: число четное \n
+      У Вас: ${balls.player} \n
+      У Бота: ${balls.computer}`);
+          stepBot();
+        }
+        if ((usNum % 2 !== 0) && (botNum % 2 !== 0)) {
+          balls.addComputer(+usNum);
+          balls.takePlayer(+usNum);
+          alert(`Бот: число нечетное \n
+      У Вас: ${balls.player} \n
+      У Бота: ${balls.computer}`);
+          stepBot();
+        }
+        if ((usNum % 2 == 0) && (botNum % 2 !== 0)) {
+          balls.addPlayer(+usNum);
+          balls.takeComputer(+usNum);
+          alert(`Бот: число нечетное \n
+      У Вас: ${balls.player} \n
+      У Бота: ${balls.computer}`);
+          stepBot();
+        }
+        if ((usNum % 2 !== 0) && (botNum % 2 == 0)) {
+          balls.addPlayer(+usNum);
+          balls.takeComputer(+usNum);
+          alert(`Бот: число четное \n
+      У Вас: ${balls.player} \n
+      У Бота: ${balls.computer}`);
+          stepBot();
+        }
+      }
+      function stepBot() {
+        {if (balls.player < 1) {
+          return alert('Бот выиграл');
+        }
+        if (balls.computer < 1) {
+          return alert('Вы выиграли');
+        }}
+        const usNum = confirm(`Число четное?`);
+        const botNum = getRandomIntInclusive(1, balls.computer);
 
-        if (
-          (user === 'камень' || user === 'к' ||
-           user === 'кам') && comp === 'ножницы' ||
-            (user === 'бумага' || user === 'бум' || user === 'б') &&
-              comp === 'камень' ||
-            (user === 'ножницы' || user === 'нож' || user === 'н') &&
-            comp === 'бумага') {
+        if
+        ((usNum === true) && (botNum % 2 == 0) ||
+        (usNum === false) && (botNum % 2 !== 0)) {
+          balls.addPlayer(botNum);
+          balls.takeComputer(botNum);
+          alert(`Число Бота: ${botNum} "\n"
+          Вы + ${botNum} очков "\n"
+          У Вас: ${balls.player} "\n"
+          У Бота: ${balls.computer}`);
+          return stepPlayer();
+        } else {
+          balls.addComputer(botNum);
+          balls.takePlayer(botNum);
+          alert(`Число Бота: ${botNum} "\n"
+          Бот: + ${botNum} очков "\n"
+          У Вас: ${balls.player} "\n"
+          У Бота: ${balls.computer}`);
           return stepPlayer();
         }
-        if (user === null) {
-          return alert('Пока');
-        } else {
-          return firstStep();
-        }
-
-        function stepPlayer() {
-          if (balls.player < 1) {
-            return alert('Бот выиграл');
-          } if (balls.computer < 1) {
-            return alert('Вы выиграли');
-          }
-          const usNum = prompt(`введи число от 1 до ${balls.player}`);
-          const botNum = getRandomIntInclusive(1, 2);
-
-
-          if (usNum === null) {
-            return alert('Пока');
-          }
-          if (isNaN(usNum)) {
-            alert('Вы ввели не число');
-            stepPlayer();
-          }
-          if (usNum < 1) {
-            alert('Вы ввели отрицательное число');
-            stepPlayer();
-          }
-          if (usNum > balls.player) {
-            alert('Вы ввели число больше');
-            stepPlayer();
-          }
-          if ((usNum % 2 == 0) && (botNum % 2 == 0)) {
-            balls.addComputer(+usNum);
-            balls.takePlayer(+usNum);
-            alert(`Бот: число четное \n
-      У Вас: ${balls.player} \n
-      У Бота: ${balls.computer}`);
-            stepBot();
-          }
-          if ((usNum % 2 !== 0) && (botNum % 2 !== 0)) {
-            balls.addComputer(+usNum);
-            balls.takePlayer(+usNum);
-            alert(`Бот: число нечетное \n
-      У Вас: ${balls.player} \n
-      У Бота: ${balls.computer}`);
-            stepBot();
-          }
-          if ((usNum % 2 == 0) && (botNum % 2 !== 0)) {
-            balls.addPlayer(+usNum);
-            balls.takeComputer(+usNum);
-            alert(`Бот: число нечетное \n
-      У Вас: ${balls.player} \n
-      У Бота: ${balls.computer}`);
-            stepBot();
-          }
-          if ((usNum % 2 !== 0) && (botNum % 2 == 0)) {
-            balls.addPlayer(+usNum);
-            balls.takeComputer(+usNum);
-            alert(`Бот: число четное \n
-      У Вас: ${balls.player} \n
-      У Бота: ${balls.computer}`);
-            stepBot();
-          }
-        }
-        function stepBot() {
-          if (balls.player < 1) {
-            return alert('Бот выиграл');
-          }
-          if (balls.computer < 1) {
-            return alert('Вы выиграли');
-          }
-          const usNum = prompt(`Число четное? \n введите "да" или "нет"`);
-          const botNum = getRandomIntInclusive(1, balls.computer);
-
-          if (usNum === null) {
-            return alert('Пока');
-          }
-
-          if
-          ((usNum === 'да') && (botNum % 2 == 0)) {
-            balls.addPlayer(+botNum);
-            balls.takeComputer(+botNum);
-            alert(`Число Бота: ${botNum} \n
-             Вы + ${botNum} очков \n
-           У Вас: ${balls.player} \n
-           У Бота: ${balls.computer}`);
-            return stepPlayer();
-          }
-          if ((usNum === 'нет') && (botNum % 2 !== 0)) {
-            balls.addPlayer(+botNum);
-            balls.takeComputer(+botNum);
-            alert(`Число Бота: ${botNum} \n
-             Вы + ${botNum} очков \n
-           У Вас: ${balls.player} \n
-           У Бота: ${balls.computer}`);
-            return stepPlayer();
-          }
-          if
-          ((usNum === 'да') && (botNum % 2 !== 0)) {
-            balls.addComputer(+botNum);
-            balls.takePlayer(+botNum);
-            alert(`Число Бота: ${botNum} \n
-             Бот: + ${botNum} очков \n
-           У Вас: ${balls.player} \n
-           У Бота: ${balls.computer}`);
-            return stepPlayer();
-          }
-          if
-          ((usNum === 'нет') && (botNum % 2 == 0)) {
-            balls.addComputer(+botNum);
-            balls.takePlayer(+botNum);
-            alert(`Число Бота: ${botNum} \n
-             Бот: + ${botNum} очков \n
-           У Вас: ${balls.player} \n
-           У Бота: ${balls.computer}`);
-            return stepPlayer();
-          } else {
-            return stepBot();
-          }
-        }
-      }();
+      }
     })();
   };
   window.marbles = game;
--- lesson_final_module1/startGame.js
+++ lesson_final_module1/startGame.js
@@ -1,4 +1,4 @@
 'use strict';
 
-const startGame = window.marbles();
+const startGame = window.marbles;
 startGame();
